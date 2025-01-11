128ebe267ed76cb50b3818284648eddf0eab980c
MeteKarasubasi <meteus1923@gmail.com>
2025-01-11 21:06:19 +0300

    Update settings.py

--- Blog/blog_project/settings.py
+++ Blog/blog_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ea^x1#3wox=5r$xt784mjx@zf*z)56%@jx3g)5)%nk_2x#fz8x'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog_db',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
+        'NAME': 'xxxx_db',
+        'USER': 'xxxx',
+        'PASSWORD': 'xxxx',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -204,6 +204,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'adamwarlock628@gmail.com'
-EMAIL_HOST_PASSWORD = 'mdiv kbdf gnjr skez'
-DEFAULT_FROM_EMAIL = 'Blog <adamwarlock628@gmail.com>'
+EMAIL_HOST_USER = 'xxxxx@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxxx xxxx xxxx xxx'
+DEFAULT_FROM_EMAIL = 'Blog <xxxxx@gmail.com>'
